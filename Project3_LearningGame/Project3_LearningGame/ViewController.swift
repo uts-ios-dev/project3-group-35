e7b43e66ca5233a6442c8c666416fec02e766a59
Adam Zeaiter <adam@Adams-MacBook-Pro.local>
2018-05-22 12:06:51 +1000

    xCode File Creation
    Creation of the main file for our game.

---
+++ Project3_LearningGame/Project3_LearningGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project3_LearningGame
+//
+//  Created by Adam Zeaiter on 22/5/18.
+//  Copyright © 2018 Adam Zeaiter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
